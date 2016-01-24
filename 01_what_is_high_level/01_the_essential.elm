a316b46d7a9ab6719c647d33071219e28e9821d7
Ayman Osman <aymano.osman@gmail.com>
2016-01-24 18:35:01 +0000

    prose

--- 01_what_is_high_level/01_the_essential.elm
+++ 01_what_is_high_level/01_the_essential.elm
@@ -1,8 +1,16 @@
-import Html exposing (text)
+import Html exposing (Html, text)
 import Mouse
 
+main : Signal Html
 main =
-  Signal.map view Mouse.position
+  Signal.map (text << toString) Mouse.position
 
-view coords =
-  text (toString coords)
+-- Mouse.position : Signal (Int, Int)
+-- Html.text : String -> Html
+-- Signal.map : (a -> b) -> Signal a -> Signal b
+
+{-
+ A 'signal' is a value that changes over time.
+ Notice that what we end up with is a value of type `Signal Html` which
+ represents a component that displays the mouse position for all points in time.
+ -}
