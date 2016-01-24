d0c83219f0908384607a3a6e4c9256bb2ce85610
Ayman Osman <aymano.osman@gmail.com>
2016-01-24 20:49:45 +0000

    wip

--- 01_what_is_high_level/02_the_elm_architecture.elm
+++ 01_what_is_high_level/02_the_elm_architecture.elm
@@ -1,13 +1,40 @@
 import Html exposing (..)
+import Html.Events exposing (onClick)
 import StartApp.Simple as StartApp
 
+{-
+
+
+  Problems:
+  1) Try to add a Decrement button
+
+ -}
+
+
+main : Signal Html
 main =
   StartApp.start {model = init, view = view, update = update}
 
+
+type alias Model = Int
+
+type Action
+  = Increment
+
+
+init : Model
 init = 1
 
+view : Signal.Address Action -> Model -> Html
 view addr model =
-  div [] []
+  div []
+    [ text (toString model)
+    , div [] [button [onClick addr Increment] [text "increment"]] 
+    ]
 
+
+update : Action -> Model -> Model
 update action model =
-  model
+  case action of
+    Increment ->
+      model + 1
