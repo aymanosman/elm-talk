670ea66e0d6122630224c3a8a1e20371204c8b37
Ayman Osman <aymano.osman@gmail.com>
2016-01-30 14:13:27 +0000

    wip

--- 01_what_is_high_level/01_the_essential.elm
+++ 01_what_is_high_level/01_the_essential.elm
@@ -10,7 +10,15 @@ main =
 -- Signal.map : (a -> b) -> Signal a -> Signal b
 
 {-
- A 'signal' is a value that changes over time.
- Notice that what we end up with is a value of type `Signal Html` which
- represents a component that displays the mouse position for all points in time.
+ A 'signal' is a value that changes over time. Notice that
+ what we end up with is a value of type `Signal Html` which
+ represents a component that displays the mouse position
+ for all points in time.
+
+---
+---
+
+ type Signal α = Time → α
+
+ 'GUI' = Signal Html or 'Html over time'
  -}
