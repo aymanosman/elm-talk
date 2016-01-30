4f7499581556432d540b3240c69078dec03ee4fe
Ayman Osman <aymano.osman@gmail.com>
2016-01-30 15:17:29 +0000

    wip

--- other-stuff/circle-at-mouse.elm
+++ other-stuff/circle-at-mouse.elm
@@ -10,9 +10,19 @@ view : (Int, Int) -> Element
 view (x, y) =
   let w = 400
   in
-  collage w w
-  [ circle 10
-    |> filled red
-    |> move (-w/2, w/2)
-    |> move (toFloat x, toFloat -y)
+  [ circleAt x y
   ]
+  |> List.map (adjust w)
+  |> collage w w
+
+
+circleAt : Int -> Int -> Form
+circleAt x y =
+  circle 10
+    |> filled red
+    |> move (toFloat x, toFloat -y)
+
+
+adjust : Float -> Form -> Form
+adjust width =
+  move (-width/2, width/2)
