754c4cf2e5fa55e88f8e152831b24cfa93e0029c
Ayman Osman <aymano.osman@gmail.com>
2016-02-08 23:29:09 +0000

    http

--- 04_snappy/src-elm/Main.elm
+++ 04_snappy/src-elm/Main.elm
@@ -1,6 +1,23 @@
 module App where
 
 import Html exposing (..)
+import Http exposing (multipart, stringData)
+import Json.Decode as Json exposing ((:=))
+import Task exposing (..)
 
 main : Html
 main = text "Hello Snappy"
+
+createNewUser : String -> String -> Task Http.Error String
+createNewUser login pass =
+  let body =
+        multipart [ stringData "login" login
+                  , stringData "password" pass
+                  ]
+  in
+  Http.post Json.string "/new_user" body
+
+
+port ff : Task Http.Error String
+port ff = createNewUser "foo" "bar"
+
