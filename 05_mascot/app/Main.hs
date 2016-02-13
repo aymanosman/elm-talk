edc999bb7eb5b05e56cbab2d81e0fccb73a60da0
Ayman Osman <aymano.osman@gmail.com>
2016-02-13 16:46:56 +0000

    wip

--- 05_mascot/app/Main.hs
+++ 05_mascot/app/Main.hs
@@ -4,6 +4,7 @@ module Main where
 import Web.Scotty
 import Network.Wai.Middleware.RequestLogger
 import Network.Wai.Middleware.Static
+-- import Data.Aeson
 
 main :: IO ()
 main =
@@ -13,5 +14,8 @@ site :: ScottyM ()
 site =
   do middleware logStdoutDev
      middleware $ staticPolicy (addBase "static")
-     matchAny "/" $ text "Success"
-     matchAny "/reverse-json" $ text "Success"
+     get "/" $ file "static/index.html"
+     post "/reverse-json"
+       $ do p <- param "foo"
+            -- json $ object
+            text p
