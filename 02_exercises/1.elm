592c7bf02918055221b5e94aeb980543d1fc847d
Ayman Osman <aymano.osman@gmail.com>
2016-02-13 20:16:12 +0000

    wip

--- 02_exercises/1.elm
+++ 02_exercises/1.elm
@@ -2,7 +2,7 @@ import Html exposing (Html, text, div)
 import Mouse
 
 {-
- Display the mouse position twice!
+ Display the mouse position twice
 
  Should look like this
  (193, 125)
@@ -14,7 +14,7 @@ main : Signal Html
 main =
   Signal.map view Mouse.position
 
-view : a -> Html
+view : (Int, Int) -> Html
 view mp =
   let foo = "this is how you bind variables"
       bar = text foo
@@ -24,6 +24,7 @@ view mp =
     -- div : List Attribute -> List Html -> Html.
     -- We will ignore Attributes for now.
     -- We know how to get Html from a String with `text`.
+    -- hint: you need to use `toString` to convert data to strings
     div []
         [ div [] [bar]
         , text "something else"
