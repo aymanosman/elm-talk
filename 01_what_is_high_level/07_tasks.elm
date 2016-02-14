4b8d3816cb0e60e99922f32538bc6f1562582458
Ayman Osman <aymano.osman@gmail.com>
2016-02-14 19:51:42 +0000

    wip

--- 01_what_is_high_level/07_tasks.elm
+++ 01_what_is_high_level/07_tasks.elm
@@ -1,10 +1,31 @@
-import Task
+import Task exposing (..)
+import Http
 import Html exposing (..)
 
 main =
   Signal.map (text << toString) values
 
+{-
 
-values : Signal {currentTime : String}
+Tasks -- performing side effects
+
+-}
+
+base = "https://httpbin.org/"
+
+values : Signal (Maybe Foo)
 values =
-  foo
+  tasks.signal
+
+tasks : Signal.Mailbox (Maybe Foo)
+tasks =
+  Signal.mailbox Nothing
+
+type Foo
+  = NoOp
+  | F String
+
+port run : Signal (Task a Foo)
+port run =
+  Http.send a b c
+
