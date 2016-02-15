ff3c23a8cda386049236c8fdf0295b880d1cf379
Ayman Osman <aymano.osman@gmail.com>
2016-02-15 23:15:35 +0000

    wip

--- 01_tutorial/08_tasks.elm
+++ 01_tutorial/08_tasks.elm
@@ -1,10 +1,21 @@
 import Task exposing (..)
 import Http
+import Time
 import Html exposing (..)
 
-main : Signal Html
+main : Html
 main =
-  Signal.map (text << toString) values
+  text "Nothing to see here"
+
+
+port getDate : Task Http.Error String
+port getDate =
+  Http.getString "http://www.timeapi.org/utc/now"
+    |> Task.map logToConsole
+
+logToConsole : a -> a
+logToConsole s =
+  Debug.log "Got it " s
 
 {-
 
@@ -13,26 +24,25 @@ Tasks -- performing side effects
 As a side effect of being a 'pure' language, Elm will feel
 somewhat ceremonious when it comes to performing IO.
 
+This is probably the most seemingly 'unreasonable' part of
+Elm's API.
+
+There are two things that I would like you to understand
+from this example.
+
+1) A `Task a b` is a *description* of a side effect to
+perform. It is like a post-it note with instructions
+scribbled on it. Nothing has 'happened' yet.
+
+2) You communicate your intention for this task to run to
+the Elm runtime system by annotating the definition of the
+task with the 'port' keyword.
+
+This shows that ports are not
+only used for interop but for 'running tasks'.
+
 -}
 
-values : Signal String
-values =
-  result.signal
-
-result : Signal.Mailbox String
-result =
-  Signal.mailbox "loading..."
-
--- This is weird and will require some explanation
-port runasdasdj : Task Http.Error ()
-port runasdasdj =
-  getDate `andThen` (sendToAddress result.address)
-
-getDate : Task Http.Error String
-getDate =
-  Http.getString "http://www.timeapi.org/utc/now"
-
-sendToAddress : Signal.Address a -> a -> Task c ()
-sendToAddress addr x =
-  Signal.send addr (Debug.log "got something" x)
-
+-- foobar : Signal (Task Http.Error String)
+-- foobar =
+--   Signal.map (\_ -> getDate) (Time.every Time.second)
